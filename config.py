1ac63c7bfba79caa4d73b8e3776eb13350c5be91
DDOSBOT1223 <ownerdevil2.4@gmail.com>
2024-12-12 19:09:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Your Telegram Bot Token
+BOT_TOKEN = "7837130437:AAFoD7W9q3yAs9zQBrHN8VYRVPlAziurwZE"  # Replace with your actual bot token
+
+# Admin User ID
+ADMIN_ID = 6942423757  # Replace with the actual Telegram ID of the admin
